19d898b8f652120ea8cff98a86dceb98edc17fa2
jakubszombara <szombara.jakub@gmail.com>
2025-05-24 12:59:23 +0200

    feat: add delete & findById endpoint and pagination utils

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,16 +1,20 @@
 import { RoomsService } from './rooms.service';
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param, Delete } from '@nestjs/common';
 import { Room } from './entities/room.entity';
 import { CreateRoomRequest } from './dto/create-room.dto';
 import { RoomType } from './enums/type.enum';
+import { Pagination } from './dto/pagination.dto';
+import { RoomFilter } from './dto/roomFilter.dto';
+import { filter } from 'rxjs';
+import { PaginatedResponse } from './interfaces/paginatedResponse';  
 
 @Controller('rooms')
 export class RoomsController {
-  constructor(private readonly roomsService: RoomsService) {
+  public constructor(private readonly roomsService: RoomsService) {
     // setTimeout(async () => {
     //   console.log('start');
     //   const result = await this.create({
-    //     number: '101',
+    //     roomNumber: '101',
     //     capacity: 2,
     //     type: RoomType.DOUBLE,
     //   });
@@ -19,25 +23,40 @@ export class RoomsController {
     // lub (sudo) docker compose up (lub z --build)
   }
 
+  @Get('filtered')
+  public findFiltered(
+    @Query() filter: RoomFilter,
+    @Query() pagination: Pagination,
+  ): Promise<PaginatedResponse<Room>> {
+    return this.roomsService.findFiltered(filter, pagination);
+  }
+
   @Get()
-  findFiltered(
-    @Query()
-    filter: {
-      type?: RoomType;
-      availableFrom?: string;
-      availableTo?: string;
-    },
-    @Query() pagination: { page: 1; limit: 20 },
-  ): Promise<Room[]> {
-    return this.roomsService.findFiltered(filter as any); // +paginacja
+  public findAll(@Query() pagination: Pagination): Promise<PaginatedResponse<Room>> {
+    return this.roomsService.findAll(pagination);
   }
 
   @Post()
-  async create(@Body() roomData: CreateRoomRequest): Promise<Room> {
+  public async create(@Body() roomData: CreateRoomRequest): Promise<Room> {
     try {
       return await this.roomsService.create(roomData);
     } catch (error) {
       console.log(`[RoomsController] create: ${error}`);
+      throw error;
+    }
+  }
+
+  @Get(':id')
+  public async findRoomById(@Param('id') id: number): Promise<Room> {
+    return this.roomsService.findRoomById(id);
+  }
+
+  @Delete(':id')
+  public async deleteRoom(@Param('id') id: number): Promise<void> {
+    try {
+      return this.roomsService.deleteRoom(id);
+    } catch (error) {
+      console.log(`[RoomsController] deleteRoom: ${error}`);
       throw error;
     }
   }
